d39f51c4fe4e8a3948e566981ccf98d160ff7f79
Amro Alfien <ridzimeko@proton.me>
2025-09-07 05:12:18 +0000

    chore: remove unnecessary log

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,8 @@
 import { auth } from "@/auth";
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
-import { sampleBooks } from "@/constants";
 import { db } from "@/database/drizzle";
-import { booksTable, usersTable } from "@/database/schema";
+import { booksTable } from "@/database/schema";
 import { desc } from "drizzle-orm";
 import React from "react";
 
@@ -15,8 +14,7 @@ async function Home() {
     .from(booksTable)
     .limit(10)
     .orderBy(desc(booksTable.createdAt))) as Book[];
-  const result = await db.select().from(usersTable);
-  console.log(JSON.stringify(result, null, 2));
+  
   return (
     <>
       <BookOverview {...latestBooks[0]} userId={session?.user?.id as string} />
